8f8cb51480f57b6f74aa5c90a32a207cb122b476
Daro-Sim <daroandrot@gmail.com>
2022-09-02 11:47:50 +0700

    Fetch data added

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createHotel, deleteHotel, getSpecificHotel, updateHotel, getHotels } from "../controller/hotel.js";
+import { createHotel, deleteHotel, getSpecificHotel, updateHotel, getHotels, countByCity } from "../controller/hotel.js";
 import Hotel from "../models/Hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
@@ -17,4 +17,6 @@ router.get("/find/:id",getSpecificHotel);
 
 //Get all
 router.get("/all", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 export default router
